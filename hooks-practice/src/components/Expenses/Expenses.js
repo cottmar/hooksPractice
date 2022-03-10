3085312c0f61de0c034674e64d222958e58b6a06
cottmar <cara.ottmar@gmail.com>
2022-03-09 23:10:34 -0800

    mapping through

--- hooks-practice/src/components/Expenses/Expenses.js
+++ hooks-practice/src/components/Expenses/Expenses.js
@@ -3,20 +3,19 @@ import './Expenses.css'
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 
-const Expenses = (props) => {    
+const Expenses = (props) => {
     return (
         <Card className='expenses'>
             <div>
-                <ExpenseItem 
-                    item={props.expenses[0].item}
-                    amount={props.expenses[0].amount}
-                    uses={props.expenses[0].uses}
-                />
-                <ExpenseItem 
-                    item={props.expenses[1].item}
-                    amount={props.expenses[1].amount}
-                    uses={props.expenses[1].uses}
-                />
+                {props.expenses.map((item) => {
+                    return (
+                        <ExpenseItem 
+                            item={item.item}
+                            amount={item.amount}
+                            uses={item.uses}
+                        />
+                    )
+                })}
             </div>
         </Card>
     )
